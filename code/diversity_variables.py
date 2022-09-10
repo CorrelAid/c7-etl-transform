e74a7bd1772e033980cf8598352fb971d29cf2cc
Thomas Krannich <krannich479@googlemail.com>
2022-09-10 18:23:36 +0200

    added test scenarios

--- code/diversity_variables.py
+++ code/diversity_variables.py
@@ -1,26 +1,27 @@
+import pandas as pd
 
 
 def dv_get_amtliche_behinderung(survey_data_frame):
 
-    return survey_data_frame[ survey_data_frame["q21"] == "Ja" ]
+    return survey_data_frame["q21"] == "Ja"
 
 
 def dv_get_beeintraechtigung(survey_data_frame):
 
-    return survey_data_frame[ survey_data_frame["q18"] == "JA" ]
+    return survey_data_frame["q18"] == "Ja"
 
 
 def dv_get_schwerbehinderung(survey_data_frame):
 
-    schwerbehinderung = survey_data_frame[ (not survey_data_frame["q22"].isnull()) and (survey_data_frame["q22"] >= 50) ]
+    schwerbehinderung = ~pd.isna(survey_data_frame["q22"]) & (survey_data_frame["q22"] >= 50)
 
     # people with Schwerbehinderung have to have ticked Behinderung
-    return schwerbehinderung and dv_get_amtliche_behinderung(survey_data_frame)
+    return schwerbehinderung & dv_get_amtliche_behinderung(survey_data_frame)
 
 
 def dv_get_beeintraechtigung_oder_behinderung(survey_data_frame):
 
-    return dv_get_beeintraechtigung(survey_data_frame) or dv_get_amtliche_behinderung(survey_data_frame)
+    return dv_get_beeintraechtigung(survey_data_frame) | dv_get_amtliche_behinderung(survey_data_frame)
 
 
 def dv_set_beeintraechtigung_oder_behinderung(survey_data_frame):
@@ -29,7 +30,7 @@ def dv_set_beeintraechtigung_oder_behinderung(survey_data_frame):
 
         return False
 
-    else
+    else:
 
         survey_data_frame['dv_beeintraechtigung_oder_behinderung'] = dv_get_beeintraechtigung_oder_behinderung(survey_data_frame)
 
