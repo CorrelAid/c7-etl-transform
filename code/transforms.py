75cf685b69dccdb9eb49c22f5bf745902de8269a
vmfelso <vmfelso@users.noreply.github.com>
2022-09-10 14:20:58 +0200

    add flag_nonsensical_free_text

--- code/transforms.py
+++ code/transforms.py
@@ -30,3 +30,11 @@ def flag_critical_question_below_threshold(survey_data_frame, question):
     :return:
     """
     raise NotImplementedError
+
+def flag_nonsensical_free_text(survey_data_frame):
+    """
+
+    :param survey_data_frame:
+    :return:
+    """
+    raise NotImplementedError
