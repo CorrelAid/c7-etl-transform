060ca4da0a5b1e31d61672c0ad11d357a0d7e257
vmfelso <vmfelso@users.noreply.github.com>
2022-09-10 12:53:04 +0200

    add new load script

---
+++ code/load.py
@@ -0,0 +1,21 @@
+from sshtunnel import SSHTunnelForwarder
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+
+load_dotenv(Path(__file__).resolve().parents[1].joinpath(".env"))
+server = SSHTunnelForwarder(
+    os.environ["LIMESURVEY_SSH_IP"],
+    ssh_username=os.environ["LIMESURVEY_SSH_USER"],
+    ssh_password=os.environ["LIMESURVEY_SSH_PASSWORD"],
+    remote_bind_address=('127.0.0.1', 3306),
+    local_bind_address=('localhost', 5555)
+)
+
+server.start()
+
+print(server.local_bind_port)
+
+engine = create_engine(f"mariadb+pymysql://{os.environ['LIMESURVEY_SQL_USER']}:"
+                       f"{os.environ['LIMESURVEY_SQL_PASSWORD']}@localhost:5555/ve_limesurvey_test?charset=utf8mb4")
