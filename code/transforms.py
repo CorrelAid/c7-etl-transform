97e6b0c6359fbfde6e67f1473cedc88b60fb6921
vmfelso <vmfelso@users.noreply.github.com>
2022-09-10 13:36:36 +0200

    beginning of transforms.py

---
+++ code/transforms.py
@@ -0,0 +1,32 @@
+
+def flag_cases_without_consent(survey_data_frame):
+    """
+
+    :param survey_data_frame:
+    :return:
+    """
+    raise NotImplementedError
+
+def flag_cases_with_fake_observations(survey_data_frame):
+    """
+
+    :param survey_data_frame:
+    :return:
+    """
+    raise NotImplementedError
+
+def flag_speeders(survey_data_frame):
+    """
+
+    :param survey_data_frame:
+    :return:
+    """
+    raise NotImplementedError
+
+def flag_critical_question_below_threshold(survey_data_frame, question):
+    """
+
+    :param survey_data_frame:
+    :return:
+    """
+    raise NotImplementedError
