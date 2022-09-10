14ec09d19258d69de4718d9525d69623ffa036f3
vmfelso <vmfelso@users.noreply.github.com>
2022-09-10 15:33:09 +0200

    oops forgot to import List

--- code/transforms.py
+++ code/transforms.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+from typing import List
 
 def flag_cases_without_consent(survey_data_frame: pd.DataFrame, question_id : str):
     """
