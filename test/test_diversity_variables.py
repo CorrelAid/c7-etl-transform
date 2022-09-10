9370dfb47b829a66ca8aee876a4e430131d5be1a
vmfelso <vmfelso@users.noreply.github.com>
2022-09-10 18:54:56 +0200

    add test_diversity_variables.py

---
+++ test/test_diversity_variables.py
@@ -0,0 +1,33 @@
+"""Test most complex diversity variable calculation"""
+from code.diversity_variables import (
+    dv_get_beeintraechtigung_oder_behinderung,
+    dv_get_schwerbehinderung,
+)
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+
+@pytest.fixture(
+    params=[
+        Path(__file__).parents[0].joinpath("test_data/dv_test_schwerbehinderung.csv")
+    ]
+)
+def disability_dataframe(request):
+    dataframe = pd.read_csv(request.param)
+    yield dataframe
+
+
+def test_schwerbehinderung(disability_dataframe):
+    assert all(
+        dv_get_schwerbehinderung(disability_dataframe)
+        == disability_dataframe["schwerbehinderung"]
+    )
+
+
+def test_beeintraechtigung(disability_dataframe):
+    assert all(
+        dv_get_beeintraechtigung_oder_behinderung(disability_dataframe)
+        == disability_dataframe["behindert_oder_eingeschraenkt"]
+    )
